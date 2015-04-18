24edc62eee2fcd048318d1404bf8111bf61af155
jjh13 <joshua_horacsek@sfu.ca>
2015-04-17 17:22:21 -0700

    Adding initial project scaffolding

---
+++ kive-api/kiveapi.py
@@ -0,0 +1,18 @@
+
+
+class KiveAPI(object):
+
+    def __init__(self, username, password, auto_login=True):
+        self.
+
+    def get_datasets(self):
+        pass
+
+    def get_dataset(self, dataset_id):
+        pass
+
+    def get_pipeline_families(self):
+        pass
+
+    def get_
+
